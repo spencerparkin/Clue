d8cbb67a0ff1ed9324532e4b8158ed4f9483664b
Spencer Parkin <spencer.parkin@proton.me>
2025-08-17 22:52:10 -0600

    Note.  Way too much work.

--- Source/ClueBot/Source/Main.cpp
+++ Source/ClueBot/Source/Main.cpp
@@ -2,5 +2,7 @@
 
 int main(int argc, char** argv)
 {
+	// TODO: Get this connecting and ultimately, get it working entirely for the whole game before
+	//       you even think about trying to implement the Clue GUI client.
 	return 0;
 }
